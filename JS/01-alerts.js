9d6ca991aa1b46ae2c9b9b71d5099873966d8947
hurairarazzak <hurairarazzak125@gmail.com>
2025-07-10 21:10:42 +0500

    learned & practiced alert and variable for strings

---
+++ JS/01-alerts.js
@@ -0,0 +1,6 @@
+// alert("Welcome to my website!")
+
+// alert("It's working!");
+// alert('It\'s working!');
+
+// alert("Hey Huraira! You're on your way to becoming a JavaScript pro!")
---
+++ JS/02-vars-strings.js
@@ -0,0 +1,34 @@
+// var alphabet = "abc"
+// console.log(alphabet) // Output will be abc
+
+// var minename = "Huraira"
+// console.log(minename) // Output will be Huraira
+
+// var city = "Islamabad" // pehli dafa declare kia
+// city = "Karachi"  // dubara var nahi lagate, sirf value change karte hain
+// console.log(city); // Output will be Karachi
+
+
+// ------------------Practice-------------------
+
+// 1. Ek variable banao myName aur usmein "Huraira" save karo.
+// var myName = "Huraira";
+
+// 2. Ek variable banao nationality aur usmein "Pakistani" save karo.
+// var nationality = "Pakistani";
+
+// 3. alert() ka use karke myName ko show karo.
+// alert(myName, nationality);
+
+// 4. Variable favGame banao aur usmein "Cricket" daalo, fir alert karo.
+// var favGame = "Cricket";
+// alert(favGame);
+
+// 5. Pehle var city = "Karachi" likho, fir city = "Lahore" likho. Alert karo city.
+// var city = "Karachi";
+// city = "Lahore";
+// alert(city)
+
+// var userName = "Huraira";
+// var greeting = "Welcome to the site";
+// alert(greeting + ", " + userName);
