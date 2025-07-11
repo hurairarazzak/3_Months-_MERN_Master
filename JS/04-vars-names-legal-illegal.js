0836bec6b974308b4627901103360debe58c343b
hurairarazzak <hurairarazzak125@gmail.com>
2025-07-11 23:57:26 +0500

    learned legal & illegal variable name

---
+++ JS/04-vars-names-legal-illegal.js
@@ -0,0 +1,34 @@
+// var "userName" = "Huraira";  // This is wrong bcz we can't put string on variable name
+var userName = "Huraira"; // This is right
+
+// var 1user = "Ali" // Wrong bcz we can't start variable name with number
+var user1 = "Ali"   // This is right
+
+// var var = "hello";    // Wrong bcz JS ke keywords variable ke name nhi hoskte hae
+var alert = "hi";
+var userAlert = "hi"; // Right kyunke pura keyword nahi, bas naam mein shamil hai
+
+// var user name = "Adnan"; // Wrong bcz we can't give space in the variable name
+var userName = "Adnan";  // Right
+
+// Sirf Alphabets, digits, $ aur _ use kar sakte ho
+var myName = "Huraira";
+var $balance = 500;
+var _score = 99;
+
+// JavaScript case-sensitive hai
+
+var rose = "red";
+// console.log(Rose);  // Error Rose is not defined bcz agr apne rose define kiya hae toue exact rose hi likhna hoga na kae Rose..
+
+// camelCase: pehla word small, baqi words capital letter se start
+var userResponse = "Yes"; 
+var userResponseTime = "3s";
+
+// Readable names use karo, clarity is important
+var x = "Ali";
+var userName = "Ali";
+
+var favBrd = "Labrador";
+var favoriteBreed = "Labrador";
+
