c68182f33509861d393fedad9745c75a16e92a2b
hurairarazzak <hurairarazzak125@gmail.com>
2025-07-16 01:22:35 +0500

    5 chp completed

--- JS/05-math-familiar.js
+++ JS/05-math-familiar.js
@@ -1,9 +1,86 @@
-var num = 4;              // normal number
-var num = 2 + 2;          // math expression bhi chalega 
-alert(2 + 2);             // 4 show karega 
+var num = 4;
+var num = 2 + 2;
+// alert(2 + 2);
 
-var a = 10 + 5; // 15
-var b = 20 - 7; // 13
-var c = 3 * 4;  // 12
-var d = 12 / 3; // 4
-var e = 10 % 3; // 1 (kyunke 10 ko 3 se divide karo, 9 tak jaata hai => remainder 1)
+// Whole Maths Operator used
+var a = 10 + 5   // 15
+var b = 15 - 3   // 12
+var c = 3 * 4    // 12
+var d = 12 / 3   // 4
+var e = 10 % 3   // 1 (kyunke 10 ko 3 se divide karo, 9 tak jaata hai => remainder 1)
+
+// Variable ke sath bhi kaam hota hae
+var num1 = 50;
+var num2 = 25;
+var result = num1 + num2;   // 75 
+var result2 = num1 - num2; //  25
+
+var modulusResult = 10 % 3  // 1 
+var modulusResult2 = 9 % 3  // 0 (kyunke 3 se evenly divide ho gaya)
+
+// ------------ Easy Level Practice: ------------
+
+// 1. var sum = 12 + 8; → console par result show karo
+var sum = 12 + 8;  
+console.log(sum);    // 20
+
+// 2. var result = 100 - 45; → console par print karo
+var result = 100 - 45;
+console.log(result);   // 55
+
+// 3. var total = 9 * 7; → product print karo
+var total = 9*7
+console.log(total);  // 63
+
+// 4. var divide = 81 / 9; → output?
+var divide = 81/9;
+console.log(divide); // 9
+
+// 5. var rem = 15 % 4; → remainder kya aayega?
+var rem = 15 % 4
+console.log(rem); // 3
+
+
+// --------------- Intermediate Logic Practice: --------------- 
+// var a = 20; var b = 10;
+// sum
+// difference
+// multiply
+// divide
+// modulus
+// Sab print karo with console.log()
+
+var a = 20;
+var b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+
+// ------------------- Real-Life Inspired Challenge: ------------------- 
+// 1. Tumhare paas 3 pens hain. Har pen 25 rupees ka hai. Total price calculate karo using multiplication.
+var totalPen = 3;
+var penPrice = 25
+console.log(totalPen * penPrice); // 75
+
+// 2. Tum 200 rupees le kar shop pe gaye. Tumne 3 pens liye (har pen 25 rupees ka). Kitne rupees bach gaye?
+// Output is 195
+
+
+// ------------ Final Task for This Chapter: ------------
+// var pricePerItem = 30;
+// var quantity = 4;
+// var totalCost = pricePerItem * quantity;
+// var cashGiven = 150;
+// var change = cashGiven - totalCost;
+// console.log("Total bill: " + totalCost);
+// console.log("Bacha hua paisa: " + change);
+
+var pricePerItem = 30;
+var quantity = 4;
+var totalCost = pricePerItem * quantity;
+var cashGiven = 150;
+var change = cashGiven - totalCost;
+console.log("Total Bill: " + totalCost);
+console.log("Bacha hua paisa: " + change);
+
